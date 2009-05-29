88f69d8ae47dd035ad4e7363f58079e5dd3b1aa3
francesco.burato <francesco.burato@390deff6-4c7e-11de-813f-f783bbce749d>
2009-05-29 19:05:11 +0000

    Really first edition!

---
+++ trunk/Cryptography/src/engimasimulator/core/Riflessore.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package engimasimulator.core;
+
+/**
+ *
+ * @author francescoburato
+ */
+public class Riflessore implements Rotore{
+	private int[] array;
+	private final int size = 26;
+
+	public Riflessore(){
+		this.array = new int[this.size];
+		for(int i = 0; i < this.size; ++i)
+			this.array[i] = this.size-1-i;
+	}
+
+	public int forwardEncoding(int c) {
+		return c >= 0 && c <= this.size ? this.array[c] : 0;
+	}
+
+	public int backwardEncoding(int c){
+		return c >= 0 && c <= this.size ? this.array[c] : 0;
+	}
+}
